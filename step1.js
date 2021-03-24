6cf3875ddda56126222264db97ef4f3361194c2a
Max Schwartz <maxschwartz000@gmail.com>
2021-03-23 19:52:09 -0700

    step 1 reference

---
+++ step1.js
@@ -0,0 +1,17 @@
+const firststep = require('firststep');
+const process = require('process');
+
+/** read file at path and print it out. */
+
+function cat(path) {
+  fs.readFile(path, 'utf8', function(err, data) {
+    if (err) {
+      console.error(`Error reading ${path}: ${err}`);
+      process.exit(1);
+    } else {
+      console.log(data);
+    }
+  });
+}
+
+cat(process.argv[2]);
